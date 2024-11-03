2f9a33afd60c9ffddb679a42eb56464d16be0fd1
4ndrelim <andre_lin@u.nus.edu>
2024-11-03 13:49:34 +0800

    fix bug in dataloader and upload saved model

--- models/experimental/byte_level_copy/custom_embedder/dataset_preparation/dataset_loader.py
+++ models/experimental/byte_level_copy/custom_embedder/dataset_preparation/dataset_loader.py
@@ -18,13 +18,15 @@ class EndByteClassifierDataset(Dataset):
         self.context_window = config['model']['context_window']
         self.device = config["general"]["device"]
 
-        self.dataset_len = len(self.x_data) - self.context_window + 1
+        # need to double check with calvin if this is intended!
+        self.dataset_len = (len(self.x_data) - self.context_window + 1) // self.context_window
         assert len(self.x_data) == len(self.y_data), "Input and label files must have the same length."
 
     def __len__(self):
         return self.dataset_len
 
     def __getitem__(self, idx):
-        x = torch.tensor(self.x_data[idx : idx + self.context_window], dtype=torch.long, device=self.device)
-        y = torch.tensor(self.y_data[idx : idx + self.context_window], dtype=torch.float32, device=self.device)
+        start_idx = idx * self.context_window
+        x = torch.tensor(self.x_data[start_idx : start_idx + self.context_window], dtype=torch.long, device=self.device)
+        y = torch.tensor(self.y_data[start_idx : start_idx + self.context_window], dtype=torch.float32, device=self.device)
         return x, y
